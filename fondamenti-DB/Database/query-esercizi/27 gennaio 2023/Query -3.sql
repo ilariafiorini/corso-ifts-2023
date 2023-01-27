02770d41f643fc8b2250831dc38422b5e8f0f902
ilaria <ilaria@ilariafiorini.it>
2023-01-27 12:50:30 +0100

    aggiunti altri esercizi di DB

--- fondamenti-DB/Database/query-esercizi/27 gennaio 2023/Query -3.sql
+++ fondamenti-DB/Database/query-esercizi/27 gennaio 2023/Query -3.sql
@@ -2,4 +2,26 @@ SELECT *
 FROM prenotazioni
 WHERE importo > 100 
 AND caparra < 50
-AND (tipo_struttura = '2 stelle' OR tipo_struttura = '3 stelle')
+AND (tipo_struttura = '2 stelle' 
+OR tipo_struttura = '3 stelle');
+
+SELECT nome, cognome, dataNascita
+FROM clienti
+WHERE (nome = 'mario' OR nome = 'giorgia' OR nome = 'luigi')
+AND (cognome = 'rossi' OR cognome = 'bianchi' OR cognome = 'rossini');
+
+SELECT arrivo, partenza, importo/100*22
+FROM prenotazioni;
+
+SELECT arrivo, partenza, importo/100*22 AS iva
+FROM prenotazioni;
+
+SELECT arrivo, camera, (importo - caparra) AS saldo
+FROM prenotazioni
+WHERE (tipo_struttura = '2 stelle' OR tipo_struttura = '3 stelle' or tipo_struttura = '4 stelle');
+
+
+
+
+
+
