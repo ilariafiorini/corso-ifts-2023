d86f645d70b4f7aa6c0a1be1fc4f4814f394a717
ilaria <ilaria@ilariafiorini.it>
2023-03-08 13:03:21 +0100

    file typescript sui tipi

---
+++ Linguaggi di Scripting 2/Tipi/tipi.ts
@@ -0,0 +1,23 @@
+let s: string;
+
+s = "hello";
+s=4;
+
+s= {
+    nome: "Giuseppe",
+    cognome: "Garibaldi"
+};
+
+let s2: string = "hello"; 
+let s3 = "hello"; 
+
+s3 = 4;
+
+let n: number;
+n = 4;
+n = "hello";
+n = {
+    nome: "Giuseppe",
+    cognome: "Garibaldi"
+};
+
