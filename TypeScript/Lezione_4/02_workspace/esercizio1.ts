3498a1a652dca552e7654911d91ea55dc75d416f
ilaria <ilaria@ilariafiorini.it>
2023-03-22 14:28:13 +0100

    ancra TypeScript

--- TypeScript/Lezione_4/02_workspace/esercizio1.ts
+++ TypeScript/Lezione_4/02_workspace/esercizio1.ts
@@ -11,3 +11,40 @@ let bankAccount = {
    };
    myself.bankAccount.deposit(3000);
    console.log(myself);
+
+
+   /////////////////////////////////////
+
+   class Person {
+    name: string;
+    private type: string;
+    protected age: number;
+
+    constructor(name: string, t?: string, a: number = 27){
+        this.name = name;
+        if(t){
+
+            this.type = t;
+        }else{
+            this.type = "italiano";
+        }
+        this.age = a;
+    }
+
+    choSonoIo():void {
+        console.log("io sono : " + this.type + " e mi chiamo " +this.name);
+    }
+    
+   }
+
+   class Lavoratore extends Person{
+    ditta: string = "Ditta";
+    anniEsperienza:number = super.age - 20;
+   }
+
+   let p = new Person("UgoFantozzi");
+   let l: Lavoratore = new Lavoratore("Merdaccia");
+
+   console.log("p.name: ", p.name);
+   console.log("l.anniEsperienza: ",l.anniEsperienza);
+   p.choSonoIo();
