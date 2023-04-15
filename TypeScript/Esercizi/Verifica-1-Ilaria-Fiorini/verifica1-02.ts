16cddc0e0c779fe2ccf2cba102f046f643b657a0
ilaria@ilariafiorini.it <ilaria@ilariafiorini.it>
2023-04-15 11:11:07 +0200

    aggiunta roba

--- TypeScript/Esercizi/Verifica-1-Ilaria-Fiorini/verifica1-02.ts
+++ TypeScript/Esercizi/Verifica-1-Ilaria-Fiorini/verifica1-02.ts
@@ -2,15 +2,15 @@ class Accumulatore {
     private currentLoad: number;
     constructor(private maxLoad: number){
         this.currentLoad = 0;
-        console.log(this.currentLoad);
-        console.log(this.maxLoad);
+        //console.log(this.currentLoad);
+        //console.log(this.maxLoad);
     }
     
     carica(n: number){
         if((this.currentLoad+n)<=this.maxLoad){
             
             this.currentLoad += n;
-            console.log(this.currentLoad);
+            //console.log(this.currentLoad);
         } else{
             this.currentLoad=this.maxLoad;
         }
@@ -22,37 +22,42 @@ class Accumulatore {
         if((this.currentLoad-n)>=0){
             
             this.currentLoad -= n;
-            console.log(this.currentLoad);
+            //console.log(this.currentLoad);
         } else{
             this.currentLoad=0;
-            console.log(this.currentLoad);
+            //console.log(this.currentLoad);
         }
     }
     
     livello() :number {
-        console.log(this.currentLoad);
+        //console.log(this.currentLoad);
         return this.currentLoad;
 
     }
 
     livello100() :number {
         
-        console.log(this.currentLoad/this.maxLoad*100);
+        //console.log(this.currentLoad/this.maxLoad*100);
         return this.currentLoad/this.maxLoad*100;
     }
 }
 
 let accuTest = new Accumulatore(5000);
 
-console.log(accuTest.livello);
-console.log(accuTest.livello100);
+console.log("livello:"+accuTest.livello());
+console.log(accuTest.livello100()+"%");
+
 accuTest.carica(100);
-console.log(accuTest.livello);
-console.log(accuTest.livello100);
+console.log("livello:"+accuTest.livello());
+console.log(accuTest.livello100()+"%");
+
 accuTest.scarica(50);
-console.log(accuTest.livello);
-console.log(accuTest.livello100);
+
+console.log("livello:"+accuTest.livello());
+console.log(accuTest.livello100()+"%");
+
+
 accuTest.scarica(60);
-console.log(accuTest.livello);
-console.log(accuTest.livello100);
+console.log("livello:"+accuTest.livello());
+console.log(accuTest.livello100()+"%");
 
